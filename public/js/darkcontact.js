efa2ec8af7f4f2dfc32038a5369a528f0e36a13d
sai sinare <saisinare19@gmail.com>
2023-10-10 06:21:29 +0530

    delete dark mode js files

--- public/js/darkcontact.js
+++
@@ -1,7 +0,0 @@
-// body{
-//     background-color: rgb(255, 215, 215);
-//     color: black;
-// }
-const main = document.querySelector('main');
-main.style.backgroundColor = "rgb(255, 215, 215);"
-.style.backgroundColor = "rgb(255, 215, 215);"
